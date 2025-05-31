2058ea3965f5f668c8456e35703a2e90a6873975
melivasr <131542698+melivasr@users.noreply.github.com>
2025-05-31 13:25:07 -0600

    Cambios

--- server/juego/jugador.h
+++ server/juego/jugador.h
@@ -2,9 +2,15 @@
 #define JUGADOR_H
 
 #include "../util/tipos.h"
+#include <stdio.h>
+#include <string.h>
+#include "../util/constantes.h"
+#include "nivel.h"
 
 void mover_jugador(Jugador *jugador, char direccion);
-void golpear(Jugador *jugador);
+void golpear(Jugador *jugador, Nivel *nivel);
 void perder_vida(Jugador *jugador);
+void brincar_jugador(Jugador *jugador);
+void sumar_puntaje(Jugador *jugador, int puntos);
 
 #endif
--- server/util/log.h
+++ server/util/log.h
@@ -1,6 +1,7 @@
 #ifndef LOG_H
 #define LOG_H
-
+#include <stdio.h>
+#include <time.h>
 void log_info(const char *mensaje);
 void log_error(const char *mensaje);
 
