b01848c0e3d0c4cf2097bb6e02d10627b55a36d6
a-alfaro19 <a.alfaro@estudiantec.cr>
2025-06-05 11:15:28 -0600

    Struct para los obstaculos en el server

---
+++ server/juego/Obstacle.c
@@ -0,0 +1,53 @@
+#include "Obstacle.h"
+#include <stdlib.h>
+
+
+Obstacle* createObstacle(const ObstacleType type, const int x, const int y) {
+    Obstacle* obstacle = malloc(sizeof(Obstacle));
+
+    // Set Dir
+    switch (type) {
+        case YETI:
+            obstacle->dir = x == 0 ? RIGHT : LEFT;
+            break;
+
+        case BIRD:
+            obstacle->dir = RIGHT;
+            break;
+
+        case ICE_BLOCK:
+            obstacle->dir = DOWN;
+            break;
+    }
+
+    obstacle->type = type;
+    obstacle->x = x;
+    obstacle->y = y;
+
+    return obstacle;
+}
+
+void moveObstacle(Obstacle* obstacle) {
+    switch (obstacle->type) {
+        case YETI:
+            // Always moves horizontally
+            obstacle->x += obstacle->dir == RIGHT ? 1 : -1;
+            break;
+
+        case BIRD:
+            // Set new Y
+            const Dir validDirs[] = {UP, DOWN};
+            const int dirArraySize = sizeof(validDirs) / sizeof(Dir);
+            const Dir randomDir = validDirs[rand() % dirArraySize];
+            obstacle->y += randomDir == UP ? -1 : 1;
+
+            // Set new X
+            obstacle->x += 1;
+            break;
+
+        case ICE_BLOCK:
+            // Always moves down
+            obstacle->y += 1;
+            break;
+    }
+}
